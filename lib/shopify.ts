33ad7371480f127297256c6b58e464bfbd8f696e
Will Ye <williamyeny@gmail.com>
2022-02-03 12:09:04 -0500

    Add app bridge

--- lib/shopify.ts
+++ lib/shopify.ts
@@ -1,7 +1,7 @@
 import Shopify, { ApiVersion, ContextParams } from "@shopify/shopify-api";
 
 const context = {
-  API_KEY: process.env.SHOPIFY_API_KEY ?? "",
+  API_KEY: process.env.NEXT_PUBLIC_SHOPIFY_API_KEY ?? "",
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET ?? "",
   SCOPES: process.env.SHOPIFY_SCOPES?.split(",") ?? [],
   HOST_NAME: process.env.HOST ?? "",
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,39 @@
 import "@shopify/polaris/build/esm/styles.css";
 import type { AppProps } from "next/app";
 import { PolarisProvider } from "providers/PolarisProvider";
+import { useEffect, useState } from "react";
+import { Provider as AppBridgeProvider } from "@shopify/app-bridge-react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apiKey = process.env.NEXT_PUBLIC_SHOPIFY_API_KEY;
+  const [host, setHost] = useState<string>();
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const host = url.searchParams.get("host");
+
+    // If host is not set, than the page is being loaded outside of App Bridge
+    // so we should proceed with starting OAuth
+    if (host) {
+      setHost(host);
+    } else {
+      window.location.pathname = `/api/auth/shopify/login`;
+    }
+  }, []);
+
+  if (!apiKey) {
+    return "Error: no Shopify API key found.";
+  }
+
+  if (!host) {
+    return <div>Redirecting...</div>;
+  }
+
   return (
     <PolarisProvider>
-      <Component {...pageProps} />
+      <AppBridgeProvider config={{ apiKey, host, forceRedirect: true }}>
+        <Component {...pageProps} />
+      </AppBridgeProvider>
     </PolarisProvider>
   );
 }
